693e57ac86db767be41935d61988a1b7a721de93
fabricio <fabricio.jacob@outlook.com>
2020-08-15 20:26:19 -0300

    add

--- kafka-stream/src/main/java/com/github/fabriciolfj/stream/flashsale/FlashSaleVoteTwoValueTransformer.java
+++ kafka-stream/src/main/java/com/github/fabriciolfj/stream/flashsale/FlashSaleVoteTwoValueTransformer.java
@@ -2,13 +2,14 @@ package com.github.fabriciolfj.stream.flashsale;
 
 import com.github.fabriciolfj.domain.integration.broker.model.FlashSaleVoteMessage;
 
+import com.github.fabriciolfj.domain.integration.broker.model.FlashSaleVoteMessageNew;
 import com.github.fabriciolfj.util.LocalDateTimeUtil;
 import org.apache.kafka.streams.kstream.ValueTransformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
 
 import java.time.LocalDateTime;
 
-public class FlashSaleVoteTwoValueTransformer implements ValueTransformer<FlashSaleVoteMessage, FlashSaleVoteMessage> {
+public class FlashSaleVoteTwoValueTransformer implements ValueTransformer<FlashSaleVoteMessage, FlashSaleVoteMessageNew> {
 
     private final long voteStartTime;
     private final long voteEndTime;
@@ -25,9 +26,16 @@ public class FlashSaleVoteTwoValueTransformer implements ValueTransformer<FlashS
     }
 
     @Override
-    public FlashSaleVoteMessage transform(FlashSaleVoteMessage value) {
+    public FlashSaleVoteMessageNew transform(FlashSaleVoteMessage value) {
         var recordTime = this.context.timestamp();
-        return (recordTime >= voteStartTime && recordTime <= voteEndTime) ? value : null;
+        return (recordTime >= voteStartTime && recordTime <= voteEndTime) ? toNew(value) : null;
+    }
+
+    private FlashSaleVoteMessageNew toNew(FlashSaleVoteMessage value) {
+        var flash = new FlashSaleVoteMessageNew();
+        flash.setCustomerId(value.getCustomerId());
+        flash.setItemName(value.getItemName());
+        return flash;
     }
 
     @Override
